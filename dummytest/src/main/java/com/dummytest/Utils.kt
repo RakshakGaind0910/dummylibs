138159bfb1cc2f5a484d86d25f543ffd902bfb72
RakshakGaind0910 <rakshakgaind0910@gmail.com>
2022-12-01 13:50:09 +0530

    update

--- dummytest/src/main/java/com/dummytest/Utils.kt
+++ dummytest/src/main/java/com/dummytest/Utils.kt
@@ -1,6 +1,11 @@
 package com.dummytest
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
 
 fun Context.toast(s: String) = Toast.makeText(this, s, Toast.LENGTH_SHORT).show()
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
